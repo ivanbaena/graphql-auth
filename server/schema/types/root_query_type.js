91319476b4b7e41893df9e574d089ba38960985f
Ivan Baena <baenadev.io@gmail.com>
2020-07-10 23:57:19 -0400

    move client side

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -5,7 +5,12 @@ const UserType = require('./user_type');
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID },
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      },
+    },
   },
 });
 
